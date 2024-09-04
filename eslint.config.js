1191440be153c9d54b9cac03c464cd16408b99a4
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-09-04 20:40:06 -0300

    feat: create editorconfig file

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,16 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'  // Adicione esta linha
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettier,  // Adicione esta linha
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,6 +28,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "prettier/prettier": ["error"]
     },
   },
 )
