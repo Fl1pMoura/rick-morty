e51f1e8454f0372ca8f22bff94a39a81c24e176a
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-10-30 20:32:54 -0300

    fix: remover botão

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,9 +12,6 @@ export function Header() {
         <LinkComponent classname="w-full max-w-80 lg:w-auto" to={"/about"}>
           About me
         </LinkComponent>
-        <LinkComponent classname="w-full max-w-80 lg:w-auto" to={"/about"}>
-          About me
-        </LinkComponent>
       </nav>
     </header>
   );
